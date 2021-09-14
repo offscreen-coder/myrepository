4eaf889de1dcf5bb08c8a36677ad6a09e89e7cef
offscreen-coder <90690029+offscreen-coder@users.noreply.github.com>
2021-09-14 16:01:59 +0530

    Add files via upload
    This is my First Java File which I created.

---
+++ FirstApplication.java
@@ -0,0 +1,5 @@
+class FirstApplication{
+	public static void main(String[] args) {
+		System.out.println("This is my first application in java."+ " " +"I am learning java."+ " "+"A message: Hello From Java");
+	}
+}
